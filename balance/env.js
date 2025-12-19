ad33e66e133876f156423f313d7030eae6d3154b
Vikas Kanchagar Belagal <steelmanwifi@gmail.com>
2025-12-19 12:58:20 +0530

    Delete balance/env.js

--- balance/env.js
+++
@@ -1,5 +0,0 @@
-// env.js (DO NOT expose in public repo)
-window.UN_CONFIG = {
-  SHEET_ENDPOINT: "https://script.google.com/macros/s/AKfycbwWM8TiraKBXHX4VU1RKjp8vC87VAErmXLzDCLF2B5TIkNze2eZvZ7gwKwRZmFmj6GSYA/exec",
-  API_KEY: "balance_sheet_secret"
-};
